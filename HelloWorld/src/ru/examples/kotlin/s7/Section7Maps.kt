d6914fef6b5c0bb8b0e1ab15d51f90f38cb7967d
homeuser <zowie_i@mail.ru>
2020-08-18 18:56:28 +0300

    Destructuring Declaration (Pattern Matching ??)

--- HelloWorld/src/ru/examples/kotlin/s7/Section7Maps.kt
+++ HelloWorld/src/ru/examples/kotlin/s7/Section7Maps.kt
@@ -24,6 +24,28 @@ fun main() {
     //{1=Car(name=Hyundai, year=2010), 2=Car(name=Ford, year=2009), 3=Car(name=Hyundai, year=2009)}
     println(hashMapOf)
     println(hashMapOf.javaClass)  //class java.util.HashMap
+
+    // Destructuring Declaration (Pattern Matching ??)
+    val p = Pair(10, "val=10")
+    val (cons, corr) = p
+    println("(${cons},${corr})") // (10,val=10)
+
+    for ((_key, _val) in mapOf) {
+        println("entry: ${_key} => ${_val}")
+    }
+
+    val (color, name, year) = car1
+    println("Car (${color}, ${name}, ${year})")
+
+    val (color2, name2, year2) = Car2("blue", "Toyota", 2008)
+    println("Car2 (${color2}, ${name2}, ${year2})")
 }
 
+// For data classes componentX() are implemented for free!
 data class Car(val color: String, val name: String, val year: Int) {}
+// in ordinary class you must implement componentX() func to make use DD
+class Car2(val color: String, val name: String, val year: Int) {
+    operator fun component1() = color
+    operator fun component2() = name
+    operator fun component3() = year
+}
