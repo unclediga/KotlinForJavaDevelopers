e460695e5d47d71f3553808c364d3772aad6431c
homeuser <zowie_i@mail.ru>
2020-09-28 15:11:50 +0300

    Nullability when using Java from Kotlin - 2

--- HelloWorld/src/ru/examples/kotlin/s9/CallJava.kt
+++ HelloWorld/src/ru/examples/kotlin/s9/CallJava.kt
@@ -6,7 +6,7 @@ fun main() {
     var car = Car("blue","Ford",2015)
 
     // No Errors
-    car.model = null;
+    //car.model = null;
 
     // No Errors
     car.color ="green";
@@ -14,7 +14,26 @@ fun main() {
     // RunTime Error
     // Exception in thread "main" java.lang.IllegalArgumentException:
     // Argument for @NotNull parameter 'color' of ru/example/java/s9/Car.setColor must not be null
-    car.color = null;
 
+    //car.color = null;
+
+    /////////////////////
+    var model1 = car.model
+    println(model1.javaClass) // java.lang.String
+    model1 = null
+    println(car)
+
+    var model2:String = car.model
+    println(model2.javaClass) // java.lang.String
+    // Compilation error:
+    //model2 = null
+    println(model2)
+
+    var model3:String? = car.model
+    // Compilation error:   because (?) - exclusively Kotlin class (String?:Any), not Java-class
+    //Kotlin: Only safe (?.) or non-null asserted (!!.) calls are allowed on a nullable receiver of type String?
+    //println(model3.javaClass)
+    model3 = null
+    println(model3)
 
 }
