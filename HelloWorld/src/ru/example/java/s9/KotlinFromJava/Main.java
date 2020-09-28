570ad2a8206b8b2fc3b5d3e89d19fbb1fc808529
homeuser <zowie_i@mail.ru>
2020-09-28 18:37:00 +0300

    Calling Kotlin Functions from Java: file name and class name are the same.

--- HelloWorld/src/ru/example/java/s9/KotlinFromJava/Main.java
+++ HelloWorld/src/ru/example/java/s9/KotlinFromJava/Main.java
@@ -1,9 +1,9 @@
 package ru.example.java.s9.KotlinFromJava;
 
-import ru.examples.kotlin.s9.KotlinFromJava.CarKt;
+import ru.examples.kotlin.s9.KotlinFromJava.StaticCar;
 
 public class Main {
     public static void main(String[] args) {
-        CarKt.topLevel();
+        StaticCar.topLevel();
     }
 }
--- HelloWorld/src/ru/examples/kotlin/s9/KotlinFromJava/Car.kt
+++ HelloWorld/src/ru/examples/kotlin/s9/KotlinFromJava/Car.kt
@@ -1,7 +1,8 @@
+@file:JvmName("StaticCar")
 package ru.examples.kotlin.s9.KotlinFromJava
 
 fun topLevel() = println("I am in the Car file!")
 
-class Car(val color:String, val model:String, val year:Int){
+class CarKt(val color:String, val model:String, val year:Int){
 
 }
