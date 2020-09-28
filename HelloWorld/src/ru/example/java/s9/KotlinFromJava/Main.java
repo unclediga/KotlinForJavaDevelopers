ad607b8087f2393e88c70fbaa45db34c8eb1db46
homeuser <zowie_i@mail.ru>
2020-09-28 18:53:22 +0300

    Calling Kotlin Functions from Java: access to the object properties.

--- HelloWorld/src/ru/example/java/s9/KotlinFromJava/Main.java
+++ HelloWorld/src/ru/example/java/s9/KotlinFromJava/Main.java
@@ -1,10 +1,19 @@
 package ru.example.java.s9.KotlinFromJava;
 
+import ru.examples.kotlin.s9.KotlinFromJava.Car;
 import ru.examples.kotlin.s9.KotlinFromJava.StaticCar;
 
 public class Main {
     public static void main(String[] args) {
         StaticCar.topLevel();
         StaticCar.print("print java string");
+
+        final Car car = new Car("blue", "BMW", 2011);
+        System.out.println(car.getModel());
+        // Compile error: val color -> no setters
+        // System.out.println(car.setModel());
+        System.out.println(car.getColor());
+        car.setColor("grey");
+        System.out.println(car.getColor()); // "ever green!"
     }
 }
--- HelloWorld/src/ru/examples/kotlin/s9/KotlinFromJava/Car.kt
+++ HelloWorld/src/ru/examples/kotlin/s9/KotlinFromJava/Car.kt
@@ -3,8 +3,11 @@ package ru.examples.kotlin.s9.KotlinFromJava
 
 fun topLevel() = println("I am in the Car file!")
 
-class Car(val color:String, val model:String, val year:Int){
-
+class Car(color:String, val model:String, val year:Int){
+    var color:String = color
+    set(value){
+        field = "ever green!"
+    }
 }
 
 fun String.print(){
