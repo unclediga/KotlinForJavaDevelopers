8b858eab1cd000dd002b8eaa9145cbb17104a42f
homeuser <zowie_i@mail.ru>
2020-09-28 18:32:45 +0300

    Calling Kotlin Functions from Java: simple

---
+++ HelloWorld/src/ru/example/java/s9/KotlinFromJava/Main.java
@@ -0,0 +1,9 @@
+package ru.example.java.s9.KotlinFromJava;
+
+import ru.examples.kotlin.s9.KotlinFromJava.CarKt;
+
+public class Main {
+    public static void main(String[] args) {
+        CarKt.topLevel();
+    }
+}
---
+++ HelloWorld/src/ru/examples/kotlin/s9/KotlinFromJava/Car.kt
@@ -0,0 +1,7 @@
+package ru.examples.kotlin.s9.KotlinFromJava
+
+fun topLevel() = println("I am in the Car file!")
+
+class Car(val color:String, val model:String, val year:Int){
+
+}
