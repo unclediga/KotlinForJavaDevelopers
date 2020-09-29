ac58b337294380b06ecdc431c36ef13da34d5621
homeuser <zowie_i@mail.ru>
2020-09-29 18:03:02 +0300

    Challenge 9 (II): solution

--- HelloWorld/src/ru/example/java/s9/challenge2/Main.java
+++ HelloWorld/src/ru/example/java/s9/challenge2/Main.java
@@ -2,17 +2,26 @@ package ru.example.java.s9.challenge2;
 
 import ru.examples.kotlin.s9.challenge2.Challenge;
 import ru.examples.kotlin.s9.challenge2.Employee;
+import ru.examples.kotlin.s9.challenge2.KotlinStuff;
 
 public class Main {
     public static void main(String[] args) {
 
+        // Rename Kotlin file from default 'KotlinCodeKt' -> 'KotlinStuff'
         KotlinStuff.sayHelloToJava("Student");
 
         final Employee employee = new Employee("John", "Smith", 2018);
+        // In Kotlin file change Property 'startYear':
+        // 1) val -> var
+        // 2)  add @JvmField for field-like access
         employee.startYear = 2019;
 
+        // Force Kotlin compiler generate static method 'doMath(int, int)', but not instance method
         Challenge.doMath(4, 5);
 
+        // fix it!
+        // 'takesDefault(java.lang.String, java.lang.String)' in
+        // 'ru.examples.kotlin.s9.challenge2.Employee' cannot be applied to '(java.lang.String)'
         employee.takesDefault("par1");
     }
 }
--- HelloWorld/src/ru/examples/kotlin/s9/challenge2/KotlinCode.kt
+++ HelloWorld/src/ru/examples/kotlin/s9/challenge2/KotlinCode.kt
@@ -1,3 +1,6 @@
+@file:JvmName("KotlinStuff")
+// rename Kotlin file from default 'KotlinCodeKt' -> 'KotlinStuff'
+
 package ru.examples.kotlin.s9.challenge2
 
 fun sayHelloToJava(name: String) {
@@ -5,11 +8,20 @@ fun sayHelloToJava(name: String) {
 }
 
 object Challenge {
-    fun doMath(x: Int, y: Int) = x * y
+    // Force Kotlin compiler generate static method 'doMath(int, int)', but not instance method
+    @JvmStatic fun doMath(x: Int, y: Int) = x * y
 }
 
-class Employee(val firstName: String, val lastName: String, val startYear: Int) {
+// In Kotlin file change Property 'startYear':
+// 1) val -> var
+// 2)  add @JvmField for field-like access
+class Employee(val firstName: String, val lastName: String, @JvmField var startYear: Int) {
+    //Force compiler generate two java-signature;
+    // - void takesDefault(param1, param2)
+    // - void takesDefault(param1)
+    @JvmOverloads
     fun takesDefault(param1: String, param2: String = "default") {
         println("takes param1=$param1 and param2=$param2")
     }
+
 }
